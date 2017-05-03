acc30220b82ac0580512c01ae74763e48a196d27
Hilmar Tryggvason <hilmart@gmail.com>
2017-05-03 17:54:44 +0000

    Health bars done

--- src/Alan and Albus Adventures/Assets/Scripts/VitalityController.cs
+++ src/Alan and Albus Adventures/Assets/Scripts/VitalityController.cs
@@ -1,28 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VitalityController : MonoBehaviour 
 {
-	public float health;
+	public float maxHealth;
+	public float currentHealth;
 	public float magicResistance;
 	public float physicalResistance;
 	public bool isDead = false;
 	public bool isInvincible;
 	public bool isInvincibilityFrame;
 	public float invincibilityFrameTime;
+	public Text healthText;
+	public Slider healthSlider;
 
 	private float nextDamage;
 
 	public void Damage(float amount, bool isMagical)
 	{
-		if(!isInvincible && !isInvincibilityFrame)
+		if(!isInvincible && !isInvincibilityFrame && !isDead)
 		{
 			isInvincibilityFrame = true;
 
 			amount /= (isMagical ? magicResistance : physicalResistance);
 
-			health -= amount;
+			currentHealth -= amount;
+
+			if (gameObject.tag == "Player")
+			{
+				UpdateUI();
+			}
+
 			CheckHealth();
 
 			StartCoroutine(InvincibiltyFrame());
@@ -31,12 +41,12 @@ public class VitalityController : MonoBehaviour
 
 	public void Heal(float amount)
 	{
-		health += amount;
+		currentHealth += amount;
 	}
 
 	private void CheckHealth()
 	{
-		if (health <= 0)
+		if (currentHealth <= 0)
 		{
 			if (gameObject.tag == "Player")
 			{
@@ -53,5 +63,11 @@ public class VitalityController : MonoBehaviour
 	{
 		yield return new WaitForSeconds(invincibilityFrameTime);
 		isInvincibilityFrame = false;
+	}
+
+	private void UpdateUI()
+	{
+		healthText.text = currentHealth + "/" + maxHealth;
+		healthSlider.value = currentHealth / maxHealth;
 	}
 }
