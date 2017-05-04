dc70f75b9f20d6214ed38127fb1971dd942cab08
tdog360 <tinnaa93@gmail.com>
2017-05-04 14:04:13 +0000

    Sound managergit status

---
+++ src/Alan and Albus Adventures/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+	public AudioSource soundEffect;
+	public static SoundManager instance { get; private set; }
+
+	void Awake() {
+		instance = this;
+	}
+	
+	// Update is called once per frame
+	public void PlaySingle(AudioClip sound)
+	{
+		soundEffect.clip = sound;
+		soundEffect.Play();
+	}
+}
