5626fb1e8dd9b3e70055673b39b0c5615f9de527
tdog360 <tinnaa93@gmail.com>
2017-05-11 22:30:06 +0000

    Minimalistic Main Menu

---
+++ src/Alan and Albus Adventures/Assets/Scripts/ButtonController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour {
+
+	public void LoadFreePlay()
+	{
+		SceneManager.LoadScene("Main");
+	}
+	public void Quit()
+	{
+#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+#else
+		Application.Quit();
+#endif
+	}
+}
