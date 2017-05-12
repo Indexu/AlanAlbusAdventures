80da1e18e597c1397051c2bdac8807caba042539
tdog360 <tinnaa93@gmail.com>
2017-05-12 15:50:38 +0000

    temp menu ready

---
+++ src/Alan and Albus Adventures/Assets/Scripts/MenuNavigationHandler.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+using UnityEngine.EventSystems;
+
+public class MenuNavigationHandler : MonoBehaviour {
+	//private IList<Player> player;
+	public EventSystem eventSystem;
+	public GameObject selectedObject;
+	public bool selectedItem;
+	public void Awake()
+	{
+		//player = ReInput.players.AllPlayers;
+        ReInput.ControllerConnectedEvent += OnControllerConnected;
+	}
+
+	private void OnControllerConnected(ControllerStatusChangedEventArgs args)
+    {
+		//playstationController = args.name.Contains("Sony");
+    }
+
+	private void OnDestroy()
+    {
+        ReInput.ControllerConnectedEvent -= OnControllerConnected;
+    }
+
+	private void onDisable()
+	{
+		Debug.Log("bye");
+		selectedItem = false;
+	}
+	private void Update()
+	{
+		checkInput();
+	}
+
+	private void checkInput()
+	{
+		var analogStick = Rewired.ReInput.players.GetPlayer(0).GetAxis("Move Vertical");
+		if(analogStick < 0)
+		{
+			Debug.Log("down");
+			eventSystem.SetSelectedGameObject(selectedObject);
+			selectedItem = true;
+        }
+		if(analogStick > 0)
+		{
+			Debug.Log("up");
+			eventSystem.SetSelectedGameObject(selectedObject);
+			selectedItem = true;
+		}
+
+	}
+
+}
